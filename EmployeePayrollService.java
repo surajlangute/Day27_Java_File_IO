2bca1357e695af49582016e3b0cfe40050180a0e
surajlangute <surajlangute55@gmail.com>
2022-11-01 21:05:02 +0530

    Added PayRollService Program

---
+++ EmployeePayrollService.java
@@ -0,0 +1,15 @@
+package day27;
+
+
+	public class EmployeePayrollService {
+
+		public enum IOStream {
+			CONSOLE_IO, FILE_IO, DB_IO
+		}
+
+		public static void main(String[] args) {
+			IOStream iOStream = IOStream.CONSOLE_IO;
+
+		}		
+	}
+
